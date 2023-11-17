b8766a9f667c8f2568324a66f2267d718fb586f9
AntoineCalmettes <calmettes.antoine2@gmail.com>
2023-11-17 10:34:19 +0100

    communication back-end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
-
+import { HttpClientModule } from '@angular/common/http'; 
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +13,9 @@ import { TodoListComponent } from './todo-list/todo-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,30 +1,57 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
-
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { Task } from '../task.model';
+import { TaskService } from '../task.service';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
   @ViewChild('taskInput') taskInputRef!: ElementRef<HTMLInputElement>;
-  tasks: string[] = ["Sortir le chien"];
+
+  tasks: Task[] = [];
+  newTask: Task = { id: 0, title: '', description: '' };
+
+  constructor(private taskService: TaskService) { }
 
   errorMessage: string = '';
 
-  addTask(task: string, event: Event): void {
-    event.preventDefault(); // Empêche le comportement par défaut du formulaire
-    if (task.trim() && !this.tasks.includes(task)) {
-      this.tasks.push(task); // Ajoute la tâche à la liste
-      this.taskInputRef.nativeElement.value = '';
-    } else {
-      this.errorMessage = "La tâche existe déjà dans la liste !";
-    }
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe(tasks => {
+      this.tasks = tasks;
+    });
+  }
+  addNewTask(): void {
+
+    this.taskService.addTask(this.newTask).subscribe(() => {
+      // Réinitialise la nouvelle tâche pour permettre l'ajout d'une autre tâche
+      this.newTask = { id: 0, title: '', description: '' };
+      // Mettre à jour la liste des tâches après l'ajout réussi
+      this.taskService.getTasks().subscribe((tasks) => {
+        this.tasks = tasks;
+      });
+    });
+
+
+  }
+  fetchTasks(): void {
+    this.taskService.getTasks().subscribe(tasks => {
+      this.tasks = tasks;
+    });
   }
 
-  deleteTask(task: string): void {
-    this.tasks = this.tasks.filter(item => item !== task);
+  deleteTask(id: number): void {
+    this.taskService.deleteTask(id).subscribe(() => {
+      console.log(`Tâche avec l'ID ${id} supprimée avec succès.`);
+      this.fetchTasks(); // Mettre à jour la liste après la suppression
+    });
   }
-
-
 }
+
+
+
+// deleteTask(task: string): void {
+//   this.tasks = this.tasks.filter(item => item !== task);
+// }
+
